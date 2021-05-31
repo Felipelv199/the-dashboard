d394d3ae5abd8c7736ec498b0158f8e0efc5a62d
Felipe López Valdez <0204021@up.edu.mx>
2021-05-31 17:06:15 -0600

    Add validations to upload endpoint

--- covid_env/src/controllers/api/data.controllers.js
+++ covid_env/src/controllers/api/data.controllers.js
@@ -98,7 +98,6 @@ export const uploadFile = async (req, res) => {
   const { file } = req.files;
   const workbook = new Workbook();
   let workbookFile;
-
   try {
     workbookFile = await workbook.xlsx.load(file.data);
   } catch (error) {
@@ -107,21 +106,36 @@ export const uploadFile = async (req, res) => {
       .send({ error: 'Internal Server Error', message: error.message });
     return;
   }
-
   const sheet = workbookFile.worksheets[0];
   let sheetHeaders = {};
   let docsUploadedCounter = 0;
+  let validColumns = true;
   sheet.getRows(1, 1).forEach((row) =>
-    row.eachCell(
-      (_, colNumber) =>
-        (sheetHeaders = {
-          ...sheetHeaders,
-          [colNumber]: dataPieceModelKeys[colNumber - 1],
-        })
-    )
+    row.eachCell((cell, colNumber) => {
+      const valueParsed = cell.value.split('_').join('');
+      if (
+        valueParsed.toLowerCase() !==
+        dataPieceModelKeys[colNumber - 1].toLowerCase()
+      ) {
+        validColumns = validColumns && false;
+      }
+      sheetHeaders = {
+        ...sheetHeaders,
+        [colNumber]: dataPieceModelKeys[colNumber - 1],
+      };
+    })
   );
+  if (!validColumns) {
+    res
+      .status(400)
+      .send({
+        error: 'Bad Request',
+        message: 'Some columns are missing inside the first sheet',
+      })
+      .end();
+    return;
+  }
   const rows = sheet.getRows(2, sheet.rowCount);
-
   await Promise.all(
     rows.map(async (row) => {
       let doc = {};
@@ -129,18 +143,25 @@ export const uploadFile = async (req, res) => {
         { includeEmpty: true },
         (cell) => (doc = { ...doc, [sheetHeaders[cell.col]]: cell.value })
       );
-      const queryDoc = await DataPiece.findOne({
-        idRegistro: doc['idRegistro'],
-      });
+      let queryDoc;
+      try {
+        queryDoc = await DataPiece.findOne({
+          idRegistro: doc['idRegistro'],
+        });
+      } catch (error) {
+        res
+          .status(500)
+          .send({ error: 'Internal Server Error', message: error.message });
+        return;
+      }
       if (queryDoc === null) {
         try {
           await DataPiece.create(doc);
           docsUploadedCounter += 1;
         } catch (error) {
-          res.status(500).send({
-            error: 'Internal Server Error',
-            message: error.message,
-          });
+          res
+            .status(500)
+            .send({ error: 'Internal Server Error', message: error.message });
           return;
         }
       }
