dad42338203839b6f3b53aa11c75dd88585761a9
cubomx <cheservelram657@gmail.com>
2021-06-01 17:30:09 -0500

    UPDATE: Patient filter added

--- covid_env/src/routes/api/data.routes.js
+++ covid_env/src/routes/api/data.routes.js
@@ -4,7 +4,8 @@ import {
   uploadFile,
   getDataByAgeRange,
   getBySex,
-  getDecease
+  getDecease,
+  getPatients
 } from '../../controllers/api/data.controllers';
 
 const router = Router();
@@ -14,5 +15,6 @@ router.post('/file', uploadFile);
 router.get('/', getDataByAgeRange);
 router.get('/sex', getBySex);
 router.get('/decease', getDecease)
+router.get('/patient', getPatients);
 
 export default router;
