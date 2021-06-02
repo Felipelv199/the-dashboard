f23d762fa061dc06ff2da78b47283bd2c1cbf609
cubomx <cheservelram657@gmail.com>
2021-06-02 00:54:32 -0500

    UPDATE: Patient filter done

--- covid_env/src/routes/client.routes.js
+++ covid_env/src/routes/client.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { home, uploadFile, age } from '../controllers/client.controllers';
+import { home, uploadFile, age, patient } from '../controllers/client.controllers';
 
 const router = Router();
 
 router.get('/', home);
 router.get('/upload', uploadFile);
 router.get('/graphs/age', age);
+router.get('/graphs/patient',  patient)
 
 export default router;
